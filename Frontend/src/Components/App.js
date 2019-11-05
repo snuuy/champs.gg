dca7bc99fb842255d8e97f09e73b883bbee68b30
usephysics <rttan@edu.uwaterloo.ca>
2019-11-04 21:41:27 -0500

    adding components

--- Frontend/src/Components/App.js
+++ Frontend/src/Components/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Table from './Table.js';
 
 export default class App extends React.Component {
     render() {
         return;
     }
 }
+
+ReactDOM.render(<App/>, document.getElementById("root"));
---
+++ Frontend/src/Components/Table.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Header from './Header.js';
+import Row from './Row.js';
+
+export default class Table extends React.Component {
+    render() {
+        return;
+    }
+}
