fe7ccd5d894cabab4e431e45af3ba869e7fae4ec
usephysics <rttan@edu.uwaterloo.ca>
2019-11-07 09:03:43 -0500

    added header and riot games msg

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,23 @@ import '../css/Header.css';
 
 export default class Header extends React.Component {
     render() {
-        return;
+        return (
+            <tbody>
+                <tr className="header-row">
+                    <td className="name-header">
+                        <h1 className="header-name">Name</h1>
+                    </td>
+                    <td className="image-header">
+                        <h1 className="header-picture">Icon</h1>
+                    </td>
+                    <td className="roles-header">
+                        <h1 className="header-roles">Roles</h1>
+                    </td>
+                    <td className="rating-header">
+                        <h1 className="header-rating">Rating</h1>
+                    </td>
+                </tr>
+            </tbody>
+        );
     }
 }
