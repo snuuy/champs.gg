f9d9c3c6408b24f6f7401c51c364294b0a168ae9
daniel song <dan.w.song@gmail.com>
2019-11-11 12:03:44 -0500

    Fix sorting and add gradient colouring

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header.js';
-import returnRows from '../logic/returnRows.js';
+import Row from './Row';
+import champData from '../data/champData.js';
 import '../css/Table.css';
 
 export default class Table extends React.Component {
@@ -9,15 +10,17 @@ export default class Table extends React.Component {
         this.state = {
             search: 'Search by name...',
             roles: 'All Roles',
-            ascending: false,
+            ascending: true,
             menuOpen: false,
-            active: '↓',
+            active: '↑',
+            champions: champData
         };
         this.handleTextChange = this.handleTextChange.bind(this);
         this.resetText = this.resetText.bind(this);
         this.openRoleMenu = this.openRoleMenu.bind(this);
         this.selectRole = this.selectRole.bind(this);
         this.swapRating = this.swapRating.bind(this);
+        this.sortChampions = this.sortChampions.bind(this);
     }
 
     handleTextChange(value) {
@@ -56,7 +59,17 @@ export default class Table extends React.Component {
         this.setState({
             active: this.state.ascending ? '↓' : '↑',
             ascending: !this.state.ascending,
+            champions: this.sortChampions(this.state.champions.slice()),
         });
+    }
+
+    sortChampions(champions) {
+        if (!this.state.ascending) {
+            champions.sort((a, b) => a.rating - b.rating);
+        } else {
+            champions.sort((a, b) => b.rating - a.rating);
+        }
+        return champions;
     }
 
     render() {
@@ -70,7 +83,7 @@ export default class Table extends React.Component {
                     swapRating={this.swapRating}
                     />
                 </div>
-                {returnRows()}
+                { this.state.champions.map(champion => <Row key={champion.id} name={champion.name} icon={"https://ddragon.leagueoflegends.com/cdn/9.22.1/img/champion/" + champion.id + ".png"} roles={champion.roles.join(', ')} rating={champion.rating.toFixed(2)} />) }
             </div>
         );
     }
