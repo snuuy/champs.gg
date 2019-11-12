449fe6408491e63eddbd364019ef213cc9301bf9
usephysics <rttan@edu.uwaterloo.ca>
2019-11-12 13:07:42 -0500

    riot happy

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,19 @@ import './css/App.css';
 export default class App extends React.Component {
     render() {
         return (
-            <div className="app-table">
-                <Table/>
+            <div>
+                <h1 id="logo">
+                    champs.gg
+                </h1>
+                <div className="app-table">
+                    <Table/>
+                </div>
+                <p className="riot">
+                    champs.gg isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games
+                    or anyone <br/> officially involved in producing or managing League of Legends. League of Legends
+                    and Riot Games are <br/> trademarks or registered trademarks of Riot Games, Inc. League of Legends 
+                    © Riot Games, Inc.
+                </p>
             </div>
         );
     }
