b405b9bb1626908b2111c971b2918a48cec3cf53
usephysics <rttan@edu.uwaterloo.ca>
2019-12-01 23:41:41 -0500

    react mad

--- client/src/components/ChampInfo.js
+++ client/src/components/ChampInfo.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import RoleButton from './RoleButton.js';
-import Button from './Button.js';
-import colour from '../logic/colour.js';
 import '../css/ChampInfo.css';
 
 export default class ChampInfo extends React.Component {
@@ -11,16 +9,16 @@ export default class ChampInfo extends React.Component {
             <div className="champion-info">
                 <h1 className="champ-name-1">{this.props.name}</h1>
                 <div className="player-container">
-                    <img className="player-portrait" src={this.props.playerPicture}/>
+                    <img className="player-portrait" src={this.props.playerPicture} alt=""/>
                     <div className="player-desc">
                         <h2 className="player-name">{this.props.playerName}</h2>
                         <h2 className="player-bio">{this.props.description}</h2>
                     </div>
                     <div className="socials-container">
-                        <a href={"https://twitch.tv/" + this.props.twitch} target="_blank"><img className="social-icon" id="twitch" src="https://assets.help.twitch.tv/Glitch_Purple_RGB.png"/></a>
-                        <a href={this.props.opgg} target="_blank"><img className="social-icon" id="opgg" src="https://opgg-static.akamaized.net/images/logo/2015/reverse.rectangle.png"/></a>
-                        <a href={"https://twitter.com/" + this.props.twitter} target="_blank"><img className="social-icon" id="twitter" src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Twitter_bird_logo_2012.svg/1920px-Twitter_bird_logo_2012.svg.png"/></a>
-                        <a href={this.props.youtube} target="_blank"><img className="social-icon" id="youtube" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/YouTube_play_buttom_icon_%282013-2017%29.svg/1280px-YouTube_play_buttom_icon_%282013-2017%29.svg.png"/></a>
+                        <a href={"https://twitch.tv/" + this.props.twitch} target="_blank" rel="noopener noreferrer"><img className="social-icon" id="twitch" src="https://assets.help.twitch.tv/Glitch_Purple_RGB.png" alt=""/></a>
+                        <a href={this.props.opgg} target="_blank" rel="noopener noreferrer"><img className="social-icon" id="opgg" src="https://opgg-static.akamaized.net/images/logo/2015/reverse.rectangle.png" alt=""/></a>
+                        <a href={"https://twitter.com/" + this.props.twitter} target="_blank" rel="noopener noreferrer"><img className="social-icon" id="twitter" src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Twitter_bird_logo_2012.svg/1920px-Twitter_bird_logo_2012.svg.png" alt=""/></a>
+                        <a href={this.props.youtube} target="_blank" rel="noopener noreferrer"><img className="social-icon" id="youtube" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/YouTube_play_buttom_icon_%282013-2017%29.svg/1280px-YouTube_play_buttom_icon_%282013-2017%29.svg.png" alt=""/></a>
                     </div>
                 </div>
                 <div className="plug-container">
