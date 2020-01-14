19395e27916f0f52587212d257f1dbb70bad9f8a
bhatts39 <sunnynbhatt@gmail.com>
2020-01-13 21:05:28 -0500

    Fix serving frontend files

--- server/app.js
+++ server/app.js
@@ -4,13 +4,20 @@ const path = require("path");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "../client/build")));
 
-//routes
 const champion = require("./routes/championRoute");
 const vote = require("./routes/voteRoute");
 
+//use routes
 app.use("/api/champion", champion);
 app.use("/api/vote", vote);
+
+//serve frontend files
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+//this must be the last route
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+);
 
 app.listen(process.env.PORT || 8000);
