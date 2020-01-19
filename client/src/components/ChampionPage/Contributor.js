dedab6eb433ebcdaf034e716a4b03e14f47c3629
bhatts39 <sunnynbhatt@gmail.com>
2020-01-19 02:01:05 -0500

    Improve responsiveness

--- client/src/components/ChampionPage/Contributor.js
+++ client/src/components/ChampionPage/Contributor.js
@@ -12,26 +12,28 @@ export default class Contributor extends React.Component {
         return (
             <div className="bg-light py-3 rounded" style={{ minHeight: "150px" }}>
                 <div className="w-100">
-                    <img className="contrib-portrait float-left mr-3 ml-3" src={require('../../assets/contributors/' + info.name + '.jpg')} />
-                    <span className="d-block text-muted small text-uppercase mb-1" style={{ marginTop: "-3px" }}>
-                        Opinions from
-                    </span>
-                    <div className="contrib-name">
-                        {info.name}
-                        <div className="socials h4 d-inline-block ml-3">
-                            {info.twitter ?
-                                <a href={info.twitter} target="_blank"><i className="fab fa-twitter pr-1"></i></a> : <></>}
-                            {info.twitch ?
-                                <a href={info.twitch} target="_blank"><i className="fab fa-twitch pr-1"></i></a> : <></>}
-                            {info.instagram ?
-                                <a href={info.instagram} target="_blank"><i className="fab fa-instagram pr-1"></i></a> : <></>}
-                            {info.facebook ?
-                                <a href={info.facebook} target="_blank"><i className="fab fa-facebook pr-1"></i></a> : <></>}
-                            {info.opgg ?
-                                <a href={info.opgg} target="_blank"><i className="fas fa-user pr-1"></i></a> : <></>}
+                    <img className="contrib-portrait float-md-left mr-3 ml-3" src={require('../../assets/contributors/' + info.name + '.jpg')} />
+                    <div className="d-inline-block d-md-block align-middle">
+                        <span className="d-block text-muted small text-uppercase mb-1" style={{ marginTop: "-3px" }}>
+                            Opinions from
+                        </span>
+                        <div className="contrib-name">
+                            {info.name}
+                            <div className="socials h4 d-md-inline-block d-block ml-0 ml-md-3">
+                                {info.twitter ?
+                                    <a href={info.twitter} target="_blank"><i className="fab fa-twitter pr-1"></i></a> : <></>}
+                                {info.twitch ?
+                                    <a href={info.twitch} target="_blank"><i className="fab fa-twitch pr-1"></i></a> : <></>}
+                                {info.instagram ?
+                                    <a href={info.instagram} target="_blank"><i className="fab fa-instagram pr-1"></i></a> : <></>}
+                                {info.facebook ?
+                                    <a href={info.facebook} target="_blank"><i className="fab fa-facebook pr-1"></i></a> : <></>}
+                                {info.opgg ?
+                                    <a href={info.opgg} target="_blank"><i className="fas fa-user pr-1"></i></a> : <></>}
+                            </div>
                         </div>
                     </div>
-                    <div className="text-dark small py-0 my-0 overflow-hidden">
+                    <div className="text-dark small py-0 my-0 overflow-hidden d-block p-3 p-md-0">
                         {info.bio + " " + info.message}
                     </div>
                 </div>
