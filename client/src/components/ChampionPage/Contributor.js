79f221d3c6cfb079a0bef33fc927be607e2e6d86
usephysics <rttan@edu.uwaterloo.ca>
2020-01-18 21:17:31 -0500

    small changes

--- client/src/components/ChampionPage/Contributor.js
+++ client/src/components/ChampionPage/Contributor.js
@@ -14,21 +14,21 @@ export default class Contributor extends React.Component {
                 <div className="w-100">
                     <img className="contrib-portrait float-left mr-3 ml-3" src={require('../../assets/contributors/' + info.name + '.jpg')} />
                     <span className="d-block text-muted small text-uppercase mb-1" style={{ marginTop: "-3px" }}>
-                        Opinions by
+                        Opinions from
                     </span>
                     <div className="contrib-name">
                         {info.name}
                         <div className="socials h4 d-inline-block ml-3">
                             {info.twitter ?
-                                <a href={info.twitter}><i className="fab fa-twitter pr-1"></i></a> : <></>}
+                                <a href={info.twitter} target="_blank"><i className="fab fa-twitter pr-1"></i></a> : <></>}
                             {info.twitch ?
-                                <a href={info.twitch}><i className="fab fa-twitch pr-1"></i></a> : <></>}
+                                <a href={info.twitch} target="_blank"><i className="fab fa-twitch pr-1"></i></a> : <></>}
                             {info.instagram ?
-                                <a href={info.instagram}><i className="fab fa-instagram pr-1"></i></a> : <></>}
+                                <a href={info.instagram} target="_blank"><i className="fab fa-instagram pr-1"></i></a> : <></>}
                             {info.facebook ?
-                                <a href={info.facebook}><i className="fab fa-facebook pr-1"></i></a> : <></>}
+                                <a href={info.facebook} target="_blank"><i className="fab fa-facebook pr-1"></i></a> : <></>}
                             {info.opgg ?
-                                <a href={info.opgg}><i className="fas fa-user pr-1"></i></a> : <></>}
+                                <a href={info.opgg} target="_blank"><i className="fas fa-user pr-1"></i></a> : <></>}
                         </div>
                     </div>
                     <div className="text-dark small py-0 my-0 overflow-hidden">
