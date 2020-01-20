bb17a2c4fb777c0ef821e52ca0d6a2cf4cc02d71
usephysics <rttan@edu.uwaterloo.ca>
2020-01-19 20:40:09 -0500

    title improvement

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -15,9 +15,11 @@ export default class LandingPage extends React.Component {
         return (
             <div className="container">
                 <MetaTags>
+                    <title>champs.gg - matchups, counters, and tier list for Season 10 - League of Legends</title>
                     <meta name="description" 
                         content="counters lane matchups tier list challenger league of legends lol 
                         guide season 10 best champions climbing"/>
+                    
                 </MetaTags>
                 <div className="text-center my-md-5 my-3 pt-2">
                     <span className="display-4 h1" id="logo">
