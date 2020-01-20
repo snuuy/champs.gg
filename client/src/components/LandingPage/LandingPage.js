43a35db26042bb5e0be1df6cd4c5d4d12f89627e
usephysics <rttan@edu.uwaterloo.ca>
2020-01-19 20:26:25 -0500

    metadata

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -7,6 +7,11 @@ export default class LandingPage extends React.Component {
     render() {
         return (
             <div className="container">
+                <MetaTags>
+                    <meta name="description" 
+                        content="counters lane matchups tier list challenger league of legends lol 
+                        guide season 10 best champions climbing"/>
+                </MetaTags>
                 <div className="text-center my-md-5 my-3 pt-2">
                     <span className="display-4 h1" id="logo">
                         <img src={Logo} style={{ width: "350px" }} />
