348257f3336f4f7160cd50599303b011efcd157e
bhatts39 <sunnynbhatt@gmail.com>
2020-01-19 19:59:29 -0500

    Responsiveness changes

--- client/src/components/LandingPage/Header.js
+++ client/src/components/LandingPage/Header.js
@@ -10,14 +10,14 @@ export default class Header extends React.Component {
             <>
                 <div className="px-2 px-lg-0">
                     <div className="row bg-white rounded py-2 text-center border mb-2 mx-0 col-lg-8 col-12 mx-lg-auto">
-                        <div className="col-md-6 col-12 my-auto text-md-left text-center">
+                        <div className="col-md-5 col-12 my-auto text-md-left text-center">
                             <Search
                                 search={this.props.search}
                                 handleTextChange={this.props.handleTextChange}
                                 resetText={this.props.resetText}
                                 resetTextOffFocus={this.props.resetTextOffFocus} />
                         </div>
-                        <div className="col-md-6 col-12 text-right my-md-auto mt-md-auto mt-3 d-none d-md-block">
+                        <div className="col-md-7 col-12 text-right my-md-auto mt-md-auto mt-3 d-none d-md-block">
                             <div className="d-inline-block mr-2 align-middle">
                                 <RoleToggle
                                     role={this.props.role}
