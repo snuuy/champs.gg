dc57dabd79116ea790b3c753fbc0ae7bd50c46ed
usephysics <rttan@edu.uwaterloo.ca>
2020-02-05 14:56:52 -0500

    10.3

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
                         champs.gg
                     </a>
                     <div className="float-right text-white">
-                        <span className="small">Patch</span> 10.2
+                        <span className="small">Patch</span> 10.3
                     </div>
                 </div>
                 <BrowserRouter>
