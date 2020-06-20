28b128b3a62917025f13fb810e18dcad9bc39b4f
usephysics <55336844+usephysics@users.noreply.github.com>
2020-06-20 15:20:42 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
                         champs.gg
                     </a>
                     <div className="float-right text-white">
-                        <span className="small">Patch</span> 10.8
+                        <span className="small">Patch</span> 10.12
                     </div>
                 </div>
                 <BrowserRouter>
