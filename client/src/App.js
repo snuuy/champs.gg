a25c181ff3f96030b8688af25d1fd68aa6da4bea
usephysics <rttan@uwaterloo.ca>
2020-12-26 23:45:01 -0500

    remove patch

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
                         champs.gg
                     </a>
                     <div className="float-right text-white">
-                        <span className="small">Patch</span> 10.16
+                        <span className="small"></span>
                     </div>
                 </div>
                 <BrowserRouter>
